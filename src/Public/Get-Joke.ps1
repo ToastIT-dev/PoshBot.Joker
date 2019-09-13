075cecc48a4c66a91d9a794b9dcd74ed97874320
Joshua King <windosbk+github@gmail.com>
2019-09-14 00:52:53 +1200

    Comment based help first pass

--- src/Public/Get-Joke.ps1
+++ src/Public/Get-Joke.ps1
@@ -1,4 +1,30 @@
 function Get-Joke {
+    <#
+    .SYNOPSIS
+        Get a random joke.
+    .DESCRIPTION
+        Get a random joke, or fortune cookie message from a selection of APIs.
+    .PARAMETER Genre
+        The type of joke you're interested in. Genre can be chosen without specifying the parameter. Defaults to 'Dad' jokes.
+    .PARAMETER Random
+        Randomly selects a genre of joke to be retrieved.
+    .EXAMPLE
+        !joke
+        
+        Gets a random dad joke via default genre.
+    .EXAMPLE
+        !joke 'Dad'
+        
+        Gets a random dad joke by explictly selecting that genre.
+    .EXAMPLE
+        !joke -Genre 'Geek'
+        
+        Gets a random geek / programmer joke specifying the optional '-Genre' identifier.
+    .EXAMPLE
+        !joke -Random
+        
+        Gets a random joke from a random genre.
+    #>
     [PoshBot.BotCommand(CommandName = 'joke')]
     [CmdletBinding(DefaultParameterSetName = 'Specific')]
     param (
