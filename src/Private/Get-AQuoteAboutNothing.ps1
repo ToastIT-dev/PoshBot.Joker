1f032c852ac7b6fc7fa83621420d7abefb3b7bb8
Tony Voss <tony.voss@wwt.com>
2020-10-13 14:12:34 -0500

    Final commit to add Seinfeld quote

--- src/Private/Get-AQuoteAboutNothing.ps1
+++ src/Private/Get-AQuoteAboutNothing.ps1
@@ -2,12 +2,13 @@ function Get-AQuoteAboutNothing {
     $quoteUri = "https://seinfeld-quotes.herokuapp.com/random"
 
     try{
-        $masterOfMyDomain = Invoke-RestMethod -Method Get -Uri $quoteUri
+        # Are you master of your domain?
+        $m = Invoke-RestMethod -Method Get -Uri $quoteUri
 
-        $m = $masterOfMyDomain
         Write-Output "$($m.quote) - $($m.author) [Season $($m.season), Episode $($m.episode)]"
     }
     catch{
+        # [Slams money on the counter] I'm out!!
         Write-Output "No quote for you! Come back one year!!"
     }
 }
